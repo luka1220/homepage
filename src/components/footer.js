8eb50a804485b3eb9f184a84ddb4e41cd9afe05c
Luka Staerk <lukastaerk@luka.local>
2021-08-31 17:13:20 +0200

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,7 +12,7 @@ const Footer = () => (
     <div className="container">
       <p>
         Thanks to Gatsby and <a href="https://stackrole.com">Stackrole.com</a>,
-        who made the <a href="https://github.com/stackrole/gatsby-starter-foundation">gatsby-starter-foundation</a> that is used for this webside.
+        who made the <a href="https://github.com/stackrole/gatsby-starter-foundation">gatsby-starter-foundation</a> that is used for this <Link to="">webside</Link>.
       </p>
     </div>
   </footer>
